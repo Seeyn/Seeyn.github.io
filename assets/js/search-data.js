f2043d7493c2b4ceec1d8a4ad05d220605a7a57f
Seeyn <48508432+Seeyn@users.noreply.github.com>
2026-02-12 22:35:17 +0000

    Deploying to gh-pages from @ Seeyn/Seeyn.github.io@8c7dcb1a0b8ccd76488f7ae0f21680306bcdbf75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -546,7 +546,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ttpbA4wAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
