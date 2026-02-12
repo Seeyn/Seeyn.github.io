beaa1660e1fa30f9f1910e3598a2190a3e45dbed
Seeyn <48508432+Seeyn@users.noreply.github.com>
2026-02-12 21:39:42 +0000

    Deploying to gh-pages from @ Seeyn/Seeyn.github.io@a1d8f6b8960ff98536fb4aeceae029bff9521e7e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -507,6 +507,13 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://openreview.net/profile?id=~Zelin_Li1", "_blank");
+        },
+      },{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
@@ -518,7 +525,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%65%6C%69%6E%33@%69%6C%6C%69%6E%6F%69%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -540,13 +547,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
